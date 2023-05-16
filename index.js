a9ca46fc628e9981a4f4be806789c30d13d59c8e
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2023-05-16 22:15:55 +0100

    Update index.js
    Started to work with the OMDb API

---
+++ index.js
@@ -0,0 +1,7 @@
+async function SearchMovies(movieTitle) {
+    const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=41a510e&t=${movieTitle}`)
+    const data = await res.json()
+    console.log(data.Runtime)
+}
+
+SearchMovies('Blade Runner')
