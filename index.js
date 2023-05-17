fdd079ad779431f7fb22e092ba2630cd2d2700c2
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2023-05-17 22:39:33 +0100

    Work on API data
    Set up the structure of the JavaScript with the function needed. created the template html that will be filled in with the data returned from the get request.

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 
+const filmNameInput = document.getElementById('film-name-input')
+const searchForm = document.getElementById('search-form')
+
+searchForm.addEventListener('submit', handleSearch)
 
 async function SearchMovies(movieTitle) {
-    const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=41a510e&t=${movieTitle}`)
+    const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=41a510e&s=${movieTitle}`)
     const data = await res.json()
-    console.log(data.Runtime)
+    renderSearchList(data.Search)
+}
+
+function handleSearch(event) {
+    event.preventDefault()
+    // console.log(filmNameInput.value) remove later
+
+    if(filmNameInput.value) {
+        const searchList = SearchMovies(filmNameInput.value)
+        console.log(searchList)
+    }
+}
+
+function renderSearchList(movies) {
+    let searchListHtml = ''
+
+    if(movies) {
+        for (let movie of movies) {
+            searchListHtml += getMovieCardHtml(movie)
+        }
+    }
+}
+
+// need to search for the individual films from the api to get the plot, rating and tags
+
+function getMovieCardHtml(movie) {
+    console.log(movie)
+
+    // template of the movie card to be created from the data.
+    return `
+        <div class="movie-container">
+            <img src=${movie.Poster} />
+            <div class='movie-info'>
+                <h1>Movie Title</h1>
+                <p>Rating</p>
+                <p>Run time</p>
+                <p>Tags</p>
+                <button>Add to watch list</button>
+                <p>Plot</p>
+            </div>
+        </div>
+    `
 }
